e26b444025018965732836416262c705e039037b
Addo Michael <michaeladdo1997@gmail.com>
2024-06-04 18:33:55 +0000

    .

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,33 +1,52 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name'],
-    unique: true,
+const tourSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'A tour must have a name'],
+      unique: true,
+    },
+    slug: String,
+    rating: {
+      type: Number,
+      default: 4.5,
+    },
+    price: {
+      type: Number,
+      required: [true, 'A tour must have a price9'],
+    },
+    duration: Number,
+    difficulty: String,
+    images: [String],
+    summary: String,
+    description: String,
+    startDates: [Date],
+    nextStartDate: String,
+    maxGroupSize: Number,
+    ratingsAverage: Number,
+    ratingsQuantity: Number,
+    numReviews: Number,
+    imageCover: String,
+    createdAt: { type: Date, default: Date.now },
   },
-  rating: {
-    type: Number,
-    default: 4.5,
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
   },
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a price9'],
-  },
-  duration: Number,
-  difficulty: String,
-  // createdAt: Date().toString(),
-  images: [String],
-  summary: String,
-  description: String,
-  startDates: [String],
-  nextStartDate: String,
-  maxGroupSize: Number,
-  ratingsAverage: Number,
-  atingsQuantity: Number,
-  numReviews: Number,
-  imageCover: String,
-  createdAt: { type: Date, default: Date.now },
+);
+
+tourSchema.virtual('durationWeeks').get(function () {
+  return this.duration / 7;
+});
+
+//DOCUMENT MIDDLEWAR
+//It run on the save() and create( command)
+//The call back function would be called before a document is saved into the database
+tourSchema.pre('save', function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
 });
 
 //Creating a Model
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -8,12 +8,16 @@ const {
   checkId,
   checkBody,
   aliasTopTours,
+  getTourStats,
+  getMonthlyPlan,
 } = require('../controllers/tourController');
 
 const router = express.Router();
 
 // router.param('id', checkId); // Parameter Middleware
 router.route('/top-5-tour').get(aliasTopTours, getAllTours);
+router.route('/tour-stats').get(getTourStats);
+router.route('/monthly-plan/:year').get(getMonthlyPlan);
 
 router.route('/').get(getAllTours).post(createTour);
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
